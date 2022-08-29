5ecb4873e5eaf7cc7dcb164653aaacabc91a8217
Panni Belevski <pannibel@gmail.com>
2022-08-29 16:03:12 +0200

    working

---
+++ capitalization3.js
@@ -0,0 +1,6 @@
+const name = "Anastasia"
+const capitalize = name.substring(2, 3).toUpperCase();
+const lowercase1 = name.substring(0, 2).toLowerCase();
+const lowercase2 = name.substring(3).toLowerCase();
+
+console.log(lowercase1+capitalize+lowercase2)
