3a3a438725910dbcc063a9933c64fa94a6f13b2e
Panni Belevski <pannibel@gmail.com>
2022-09-05 13:22:13 +0200

    forloop and functions commit

---
+++ forloop.js
@@ -0,0 +1,35 @@
+/*1.
+for (let counter=0; counter<=9; counter++) {
+    console.log(counter)
+}*/
+
+/*2.
+for (let counter=1; counter<=10; counter++) {
+    console.log(counter)
+}*/
+
+/*3.
+for (let counter=10; counter>=0; counter--) {
+    console.log(counter)
+}
+console.log("liftoff")*/
+
+/*4.
+for (let counter=1; counter<=19; counter+=2) {
+    console.log(counter)
+}*/
+
+/*5.
+for (let counter=1; counter<=16777216; counter*=2) {
+    console.log(counter)
+}*/
+
+/*6.
+for (let counter=111; counter<=138; counter+=3) {
+    console.log(counter)
+}*/
+
+/*7.
+for (let counter=100; counter>=0; counter-=10) {
+    console.log(counter)
+}*/
---
+++ functions.js
@@ -0,0 +1,18 @@
+"use strict"
+
+
+//exercise 1a
+function sayHello(firstName) {
+    console.log(`Hello ${firstName}`)
+}
+
+sayHello("Panni");
+
+
+//exercise 2a
+function presentPet(firstName, animalType, animalName) {
+    console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName}`)
+}
+
+presentPet("Panni", "dog", "Laura")
+
