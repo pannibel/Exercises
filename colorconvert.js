6a6034c3ca966a370fa924bbe1c6af26023ec00b
Panni Belevski <pannibel@gmail.com>
2022-09-05 15:53:40 +0200

    hex to rgb

--- colorconvert.js
+++ colorconvert.js
@@ -4,15 +4,31 @@ let r;
 let g;
 let b;
 
-function colorConvert(cssString) {
+// CSS TO RGB
+function cssToRGB(cssString) {
     cssString = cssString.slice(4, -1);
     cssString = cssString.split(", ");
 
-    r = cssString[0];
-    g = cssString[1];
-    b = cssString[2];
+    r = parseInt(cssString[0]);
+    g = parseInt(cssString[1]);
+    b = parseInt(cssString[2]);
 
     console.log(r, g, b)
 }
 
-colorConvert("rgb(45, 32, 1)");
+cssToRGB("rgb(45, 32, 1)");
+
+
+// HEX TO RGB
+function hexToRGB(color) {
+
+    r = parseInt(color.substr(1, 2), 16);
+    g = parseInt(color.substr(3, 2), 16);
+    b = parseInt(color.substr(5, 2), 16);
+
+    console.log(r, g, b)
+}
+
+hexToRGB("#f29c12");
+
+
